7d87d8f4dac21ceea79a8e19378631e1ea9d0ca8
OfekHod <1xofekx1@gmail.com>
2020-07-01 08:48:36 +0300

    working

--- airflowlivy/dags/livydag.py
+++ airflowlivy/dags/livydag.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.providers.apache.livy.operators.livy import LivyOperator
 
 default_args = {
     'owner': 'airflow',
@@ -32,6 +33,8 @@ t1 = BashOperator(
 def response_check(response):
     return response.text != 'invalid response'
 
+
+# airflow test livydag livy_batch 2020-06-06
 t2 = SimpleHttpOperator(
     dag=dag,
     task_id="livy_batch",
@@ -44,4 +47,11 @@ t2 = SimpleHttpOperator(
     response_check=response_check
 )
 
-t3 =
+# airflow test livydag livy_batch_2 2020-06-06
+t3 = LivyOperator(
+    dag=dag,
+    task_id="livy_batch_2",
+    file="/home/livy/spark-2.4.6-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.4.6.jar",
+    class_name="org.apache.spark.examples.SparkPi",
+    polling_interval=1
+)
